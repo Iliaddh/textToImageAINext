a41da01a6683eb9d40fdd8828800d3ca7e57063d
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-08-17 11:08:15 -0400

    Vercel analyticas

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import {
   ClerkProvider,
 } from "@clerk/nextjs";
 import Navbar from "./(components)/Navbar";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
